dbe6d8c45bec56d657395144fe6d4dc633a0eca1
Pablo Amaral <ferreira_pablo@ymail.com>
2019-10-15 18:46:30 -0400

    Added import with Yaml.

--- src/FamilyTree/FamilyTreeYaml.java
+++ src/FamilyTree/FamilyTreeYaml.java
@@ -1,7 +1,11 @@
 package FamilyTree;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -9,8 +13,11 @@ import java.util.Map;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.yaml.snakeyaml.Yaml;
 
 public class FamilyTreeYaml implements FamilyTreeAdapter {
@@ -19,6 +26,100 @@ public class FamilyTreeYaml implements FamilyTreeAdapter {
 	
 	public FamilyTreeYaml(FamilyMember familyMember) {
 		this.FamilyTree = familyMember;
+	}
+	
+	@Override
+	public FamilyMember Import(String filePath) {
+		FamilyTree = null;
+		Yaml yaml = new Yaml();
+		
+		try (InputStream inputStream = new FileInputStream(filePath);) {
+			Map<String, Object> myObj = yaml.load(inputStream);
+			
+			/*
+			for (Map.Entry<String, Object> entry : myObj.entrySet())  
+			{
+				PrintEntry(entry);
+			}
+			*/
+
+			FamilyTree = ImportChildrenIterator(myObj, FamilyTree);
+		}
+		catch (FileNotFoundException e) { e.printStackTrace(); }
+		catch (IOException e1) { e1.printStackTrace(); }
+		
+		return FamilyTree;
+	}
+	
+	private FamilyMember ImportChildrenIterator(Map<String, Object> myObj, FamilyMember familyMember)
+	{
+		for (Map.Entry<String, Object> entry : myObj.entrySet()) {
+			Object currentObj = entry.getValue();
+			
+			if (currentObj instanceof LinkedHashMap) {
+				Map<String, Object> currentMap = (LinkedHashMap)currentObj;
+				
+	    		// Create a new member
+	    		String name = "";
+	    		FamilyMember childMember = null;
+	    		
+	    		for (Map.Entry<String, Object> currentChildEntry : currentMap.entrySet()) {
+	    			
+	    			switch (currentChildEntry.getKey()) {
+					case "Name":
+						name = (String)currentChildEntry.getValue();
+						break;
+					case "Gender":
+						childMember = GetFamilyMember(FamilyTree, name, (String)currentChildEntry.getValue());
+						
+						if (familyMember == null) {
+		        			//Init family
+		        			familyMember = childMember;
+		        			// Add initial object to the tree so we can search for duplicate children
+		        			FamilyTree = familyMember;
+		        		} else {
+		        			// Add a member as another one child
+		                	familyMember.addChild(childMember);
+		        		}
+						break;
+					case "Children":
+						ImportChildrenIterator((LinkedHashMap)currentChildEntry.getValue(), childMember);
+						break;
+					}
+	    		}
+	    	}
+		}
+		
+		return familyMember;
+	}
+	
+	private FamilyMember GetFamilyMember(FamilyMember familyTree, String memberName, String memberGender) {
+		FamilyMember foundFamilyMember = null;
+		
+		if (familyTree != null) {
+			//familyTree.displayChildren(0);
+			foundFamilyMember = familyTree.SearchFamilyMember(memberName);	
+		}
+
+		// If member was found on the tree return it
+		if (foundFamilyMember != null) {
+			return foundFamilyMember;
+		} else {
+			return GetNewFamilyMemberByGender(null, memberName, memberGender);
+		}
+	}
+	
+	private FamilyMember GetNewFamilyMemberByGender(FamilyMember familyMember, String name, String gender) {
+		switch (gender) {
+			case "Family":
+				return new Family(name);
+			case "Man":
+				return new Man(name);
+			case "Woman":
+				return new Woman(name);
+		}
+		
+		return new Woman(name);		
 	}
 	
 	@Override
@@ -63,11 +164,5 @@ public class FamilyTreeYaml implements FamilyTreeAdapter {
 		}
 		
 		return my_obj;
-	}
-
-	@Override
-	public FamilyMember Import(String filePath) {
-		FTUtil.Print("YAML Import called with sucess");
-		return null;
 	}
 }
